10e6f870abe18a3e7c7297867bfc94099f1996b5
adatapost <adatapost@gmail.com>
2013-08-09 19:15:00 +0530

    added

---
+++ ExtensionMethod/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Test
+{
+    static void Main()
+    {
+        string str = "The       re d         sun";
+        Console.WriteLine(str.WordCount());
+        int no = 10;
+        Console.WriteLine(no.Square());
+        decimal value = 1000;
+        Console.WriteLine(value.Square());
+
+        int[] ar = { 11, 22, 3, 4, 55 };
+        Console.WriteLine(ar.Sum());
+    }
+}
+public static class MyExtension
+{
+    public static int WordCount(this string str)
+    {
+        return str.Split(new string[] { " ", "\n", "\t" }, StringSplitOptions.RemoveEmptyEntries).Length;
+    }
+    public static ValueType Square(this ValueType n)
+    {
+        return n;
+    }
+    public static int Sum(this int[] ar)
+    {
+        int s = 0;
+        foreach (var t in ar)
+            s = s + t;
+        return s;
+    }
+}
+
+ 
---
+++ LinqCollection/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Test
+{
+    static void Main()
+    {
+        //1. Select or Identify the Datasource
+        int[] nos = { 22, 3, 1, 4, 5, -66, 7, 8, 44 };
+
+        //2. Define the query (LINQ) or expression
+        int m = 1;
+
+        IEnumerable<int> query = from no in nos
+                                 where no>m
+                                 select no;
+        //3. Execute the "query" or "expresion"
+        //   Deffered execution of query
+
+        m = 30;
+        foreach (var no in query)
+        {
+            Console.WriteLine(no);
+        }
+        m = 10;
+        foreach (var no in query)
+        {
+            Console.WriteLine(no);
+        }
+    }
+}
